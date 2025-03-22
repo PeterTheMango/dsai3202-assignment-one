a59bf7f49001ca21d070a357bcc782ed48435970
PeterTheMango <72300659+PeterTheMango@users.noreply.github.com>
2025-03-22 23:26:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.sequential import run_sequential
 from src.loop_multiprocessing import run_loop
-from src.pool_multiprocessing import run_pool, run_apply
+from src.pool_multiprocessing import run_pool_async, run_apply_async, run_apply_sync, run_pool_sync
 from src.database import run_database
 from src.concurrent import run_pool
 
 
 if __name__ == "__main__":
-    num_to_test = 10000000
+    num_to_test = int(10e7)
     max_connections = 3
     num_of_processes = 6
 
@@ -16,9 +16,13 @@ if __name__ == "__main__":
     # multiprocessing_loop_time = run_loop(num_to_test)
     print("Multiprocessing Loop Stopped As System Does Not Have Enough Resources.")
     print("========= Pool Data =========")
-    multiprocessing_pool_time = run_pool(num_to_test)
+    multiprocessing_pool_time = run_pool_sync(num_to_test)
     print("========= Pool Apply Data =========")
-    multiprocessing_pool_time = run_apply(num_to_test)
+    multiprocessing_pool_time = run_apply_sync(num_to_test)
+    print("========= Pool Data ASYNC =========")
+    multiprocessing_pool_time = run_pool_async(num_to_test)
+    print("========= Pool Apply Data ASYNC =========")
+    multiprocessing_pool_time = run_apply_async(num_to_test)
     print("========= ProcessPoolExecutor Data =========")
     multiprocessing_pool_time = run_pool(num_to_test)
     print("\n\n=======================================")
